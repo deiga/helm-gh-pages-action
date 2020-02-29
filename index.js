b9e88eca495317f5322a831cf50340ef691380c7
Timo Sand <timo.sand@iki.fi>
2020-02-29 19:11:47 +0100

    Update to work with secrets.GITHUB_TOKEN

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function run() {
     }
 
     const repo = `${github.context.repo.owner}/${github.context.repo.repo}`;
-    const repoURL = `https://${accessToken}@github.com/${repo}.git`;
+    const repoURL = `https://x-access-token:${accessToken}@github.com/${repo}.git`;
     console.log('Ready to deploy your new shiny site!');
     console.log(`Deploying to repo: ${repo} and branch: ${deployBranch}`);
     console.log(
